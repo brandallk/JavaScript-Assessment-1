7a161e40af3fb714f8766a8d03c52fa5d7f5978e
Benjamen King <brk.3rdof3kings@gmail.com>
2018-01-31 19:24:30 -0700

    finish objects.js

--- objects.js
+++ objects.js
@@ -6,4 +6,30 @@ var me = {
 	'hair color': 'brown'
 };
 
+me.name = 'Benjamen R. King';
+me.age = 37;
+me['hair color'] = 'dark brown';
+console.log(me);  // -> { name: 'Benjamen R. King', age: 37, 'hair color': 'dark brown' }
+
+
 //2. Iterate over the object to console.log the propery or key names. 
+
+function logKeys(obj) {
+	var keys = Object.keys(obj);
+	keys.forEach( key => {
+		console.log(key);
+	});
+}
+logKeys(me);  // -> 'name', 'age', 'hair color'
+
+var mappedMe = new Map([
+	['name', 'Benjamen R. King'],
+	['age', 37],
+	['hair color', 'dark brown']
+]);
+function logFromMap(map) {
+	for (var key of map.keys()) {
+		console.log(key);
+	}
+}
+logFromMap(mappedMe);  // -> 'name', 'age', 'hair color'

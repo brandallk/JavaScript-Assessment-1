5c741afde76f872ef9c06301ee46a92761a700d3
Benjamen King <brk.3rdof3kings@gmail.com>
2018-01-31 19:31:19 -0700

    finish arrays.js

--- arrays.js
+++ arrays.js
@@ -2,6 +2,8 @@
 
 var contacts = [];
 
+console.log(contacts.length);  // -> 0
+
 
 //2. Add the following people to the contacts array.
 
@@ -32,6 +34,11 @@ var tom = {
 	title: 'instructor'
 };
 
+contacts.push(jake, matt, tony, andrew, tom);
+console.log(contacts);  // -> [ { name: 'Jake Overall',email: 'jake.overall@boisecodeworks.com',title: 'founder' },{ name: 'Matt Overall',email: 'matt.overall@boisecodeworks.com',title: 'founder' },{ name: 'Mark Ohnsman',email: 'mark@boisecodeworks.com',title: 'instructor' },{ name: 'Darryl Kilzer',email: 'darryl@boisecodeworks.com',title: 'instructor' },{ name: 'Tom Day',email: 'tom@boisecodeworks.com',title: 'instructor' } ]
+
 
 //3. Woops after adding all of those people to the same contacts list you realized you need a list just the instructors. Create a new variable named instructors populate it using contacts array. 
 
+var instructors = contacts.filter( person => person.title === 'instructor' );
+console.log(instructors);  // -> [ { name: 'Mark Ohnsman',email: 'mark@boisecodeworks.com',title: 'instructor' },{ name: 'Darryl Kilzer',email: 'darryl@boisecodeworks.com',title: 'instructor' },{ name: 'Tom Day',email: 'tom@boisecodeworks.com',title: 'instructor' } ]
